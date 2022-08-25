f9771797cf9bef629c9ae05bca55139782c18407
CyberD1gp <107299282+CyberD1gp@users.noreply.github.com>
2022-08-25 11:21:37 +0300

    Добавьте файлы проекта.

---
+++ DiscordBotWPF/BotMessageLog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBotWPF
+{
+    class BotMessageLog
+    {
+        public string Date { get; set; }
+
+        public long Id { get; set; }
+
+        public string Message { get; set; }
+
+        public string Name { get; set; }
+
+        public BotMessageLog(string Date, long Id, string Message, string Name)
+        {
+            this.Date = Date;
+            this.Id = Id;
+            this.Message = Message;
+            this.Name = Name;
+        }
+
+
+    }
+}
---
+++ DiscordBotWPF/MessageClient.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+using Discord.Net;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+
+
+namespace DiscordBotWPF
+{
+    class MessageClient
+    {
+        private MainWindow main;
+
+        private DiscordSocketClient _bot;
+
+        public ObservableCollection<BotMessageLog> BotMessageLog { get; set; }
+             
+
+        //private async Task MainAsync()
+        //{
+
+        //    _bot = new DiscordSocketClient();
+        //    _bot.Log += Log;
+
+
+        //    var token = File.ReadAllText(@"C:\Users\pyatn\source\repos\SkillBox_bot\SkillBox_bot\bin\Debug\Token.txt");
+
+        //    await _bot.LoginAsync(TokenType.Bot, token);
+        //    await _bot.StartAsync();
+        //}
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        public DiscordSocketClient client(MainWindow W,string PathToken = @"C:\Users\pyatn\source\repos\SkillBox_bot\SkillBox_bot\bin\Debug\Token.txt")
+        {
+            this.BotMessageLog = new ObservableCollection<BotMessageLog>();
+            this.main = W;
+
+            _bot = new DiscordSocketClient();
+            _bot = File.ReadAllText(PathToken);
+            _bot.Log += Log;
+
+        }
+
+    }
+}
